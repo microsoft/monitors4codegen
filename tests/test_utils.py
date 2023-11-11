7abd4f1baf8532a39483ce3f6c0e982a579c3a7b
Lakshya A Agrawal <t-lakagrawal@microsoft.com>
2023-11-11 17:26:43 +0000

    Make LM tests runnable on CPU

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,3 +34,16 @@ def create_test_context(params: dict) -> Iterator[MultilspyContext]:
     finally:
         if os.path.exists(temp_extract_directory):
             shutil.rmtree(temp_extract_directory)
+
+def is_cuda_available() -> bool:
+    """
+    Returns True if CUDA is available, False otherwise
+    """
+    if torch.cuda.is_available():
+        try:
+            t = torch.rand(1).cuda()
+            t = t * 2
+            return True
+        except RuntimeError:
+            return False
+    return False
